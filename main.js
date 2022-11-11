ad335f81576e921ba72d86b59ab4b9d3aeef546e
FabricioDev1 <fabriciosantos97@gmail.com>
2022-11-10 22:29:31 -0300

    atualizei times

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function createGame(player1, hour, player2){
     `
 }
 
-function createCard(date, day, games){
+function createCard(date, day, games){    
             return `
             <div class="card">
                 <h2>${date} <span>${day}</span></h2>
@@ -22,8 +22,8 @@ function createCard(date, day, games){
 document.querySelector("#cards").innerHTML = 
               
                      
-             createCard("24/11", "quinta", createGame("switzerland", "07:00", "cameroon") + createGame("uruguay", "10:00", "southkorea")+
-            createGame("portugal", "13:00", "ghana"))
+             createCard("24/11", "quinta", createGame("portugal", "07:00", "cameroon") + createGame("uruguay", "10:00", "southkorea")+
+            createGame("brazil", "13:00", "ghana"))
                        +        
             createCard('28/11', 'segunda', createGame("brazil", "11:00", "switzerland"))+
              createCard('02/12', 'sexta', createGame("brazil", "16:00", "cameroon"))
